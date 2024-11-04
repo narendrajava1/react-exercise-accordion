7fa4a8eb7d14eb2b4148a040a8d703b58e3df663
narendra <kolli7571@gmail.com>
2024-11-04 16:39:18 +0530

    added code for accordion

--- src/AccordionItem.js
+++ src/AccordionItem.js
@@ -7,13 +7,16 @@ function AccordionItem({text, title, num,currentOpen,onOpen}) {
     const isOpen=num===currentOpen;
     const handleToggle = () => {
         onOpen(num);
+        if(isOpen&&num===currentOpen){
+            onOpen(null);
+        }
     }
     return (
         <div className="accordion">
             <div className={`item ${isOpen?"open":""}`} onClick={()=>handleToggle()}>
                 <h1 className="num">{num<9?`0${num+1}` :num+1}</h1>
                 <h1 className="title">{title}</h1>
-                <p className="icon">+</p>
+                <p className={`icon ${!isOpen?"close":""}`}>{!isOpen?"+":"-"}</p>
                 {isOpen && <p className="content">{text}</p>}
             </div>
         </div>
